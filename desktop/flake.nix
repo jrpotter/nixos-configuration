d2538ed1da614c7f95ea3d893d0ea28cfaab93fb
Joshua Potter <jrpotter2112@gmail.com>
2023-12-08 08:59:18 -0700

    Install sops.

--- desktop/flake.nix
+++ desktop/flake.nix
@@ -2,31 +2,41 @@
   description = "NixOS Flake";
 
   inputs = {
-    bootstrap.url = "github:jrpotter/bootstrap/v0.1.2";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+    bootstrap = {
+      url = "github:jrpotter/bootstrap/v0.1.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, bootstrap, ... }:
+  outputs = { nixpkgs, bootstrap, home-manager, sops-nix, ... }:
     let
       system = "x86_64-linux";
     in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
-        # Modules can be attribute sets or a function that returns an attribute set.
-        # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-with-flakes-enabled
+
         modules = [
           ./configuration.nix
+          sops-nix.nixosModules.sops
           home-manager.nixosModules.home-manager
           {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
+
               users.jrpotter = import ./jrpotter;
+
               # Used to pass non-default parameters to submodules.
               extraSpecialArgs = {
                 inherit system;
