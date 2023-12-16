48da66bcd81ad6fcdffd0e610e5ed8ef7bc9416d
Joshua Potter <jrpotter2112@gmail.com>
2023-12-16 08:16:06 -0700

    update(deimos): Start hosting the bookshelf project.

--- hive/deimos/default.nix
+++ hive/deimos/default.nix
@@ -4,6 +4,8 @@ let
     "github:jrpotter/blog/d5e3dba9f2620050365084e396bd481e68dd3795";
   portfolio = builtins.getFlake
     "github:jrpotter/portfolio/d07f24b8087f712f6d2436e2fbc4af6b56518ce6";
+  bookshelf = builtins.getFlake
+    "github:jrpotter/bookshelf/bf9888c050b7a62f58be0198af19a6de7c40b375";
 in
 {
   imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
@@ -45,6 +47,13 @@ in
         enableACME = true;
         locations."/" = {
           root = blog.packages.${system}.app;
+        };
+      };
+      "bookshelf.jrpotter.com" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          root = bookshelf.packages.${system}.app;
         };
       };
     };
