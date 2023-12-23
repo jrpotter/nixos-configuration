f0faf5d5d14ace4f97fc06e4de89b69735c78e28
Joshua Potter <jrpotter2112@gmail.com>
2023-12-23 14:17:37 -0700

    framework: Upgrade to 23.11.

--- flake.nix
+++ flake.nix
@@ -2,56 +2,45 @@
   description = "Colmena hive configuration.";
 
   inputs = {
-    # Stoat
-    nixpkgs-23_05.url = "github:NixOS/nixpkgs/nixos-23.05";
-    home-manager-23_05 = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs-23_05";
+    nixpkgs-23_11 = {
+      url = "github:NixOS/nixpkgs/nixos-23.11";
     };
-
-    # Tapir
-    nixpkgs-23_11.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager-23_11 = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs-23_11";
+    };
     sops-nix-23_11 = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs-23_11";
     };
   };
 
-  outputs = {
-    nixpkgs-23_05,
-    home-manager-23_05,
-    nixpkgs-23_11,
-    sops-nix-23_11,
-    ...
-  }:
+  outputs = { nixpkgs-23_11, home-manager-23_11, sops-nix-23_11, ... }:
     let
       system = "x86_64-linux";
-      stoat = {
-        stateVersion = "23.05";
-        pkgs = import nixpkgs-23_05 { inherit system; };
-        home-manager = home-manager-23_05;
-        sops-nix = null;
-      };
       tapir = {
         stateVersion = "23.11";
         pkgs = import nixpkgs-23_11 { inherit system; };
-        home-manager = null;
+        home-manager = home-manager-23_11;
         sops-nix = sops-nix-23_11;
       };
     in
     {
       colmena = {
         meta = {
-          nixpkgs = stoat.pkgs;
+          nixpkgs = tapir.pkgs;
           specialArgs = { inherit system; };
           nodeNixpkgs = {
-            framework = stoat.pkgs;
+            framework = tapir.pkgs;
             deimos = tapir.pkgs;
             phobos = tapir.pkgs;
           };
           nodeSpecialArgs = {
             framework = {
-              inherit (stoat) home-manager;
+              inherit (tapir) home-manager;
+            };
+            deimos = {
+              inherit (tapir) sops-nix;
             };
             phobos = {
               inherit (tapir) sops-nix;
@@ -76,17 +65,14 @@
       };
 
       packages.${system}.digital-ocean = {
-        stoat = import ./digital-ocean {
-          inherit (stoat) pkgs;
-        };
         tapir = import ./digital-ocean {
-          inherit (tapir) pkgs;
+          inherit (tapir) pkgs stateVersion;
         };
       };
 
       devShells.${system}.default =
         let
-          pkgs = stoat.pkgs;
+          pkgs = tapir.pkgs;
         in
           pkgs.mkShell {
             packages = with pkgs; [ ssh-to-age sops ];
