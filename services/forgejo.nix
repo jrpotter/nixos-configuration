c5c1757ce73290c29236488ea30300600945c3b7
Joshua Potter <jrpotter2112@gmail.com>
2023-12-23 14:46:19 -0700

    Setup forgejo at `git.jrpotter.com`.

--- services/forgejo.nix
+++ services/forgejo.nix
@@ -2,7 +2,9 @@
 {
   services = {
     forgejo.enable = true;
-    nginx.virtualHosts."forgejo.jrpotter.com" = {
+    nginx.virtualHosts."git.jrpotter.com" = {
+      forceSSL = true;
+      enableACME = true;
       locations."/" = {
         recommendedProxySettings = true;
         proxyPass = "http://127.0.0.1:3000";
