2277334558091b3a285865eaaa2e68dba68b051d
Joshua Potter <jrpotter2112@gmail.com>
2023-12-23 22:20:57 -0700

    portfolio: Bump to version `426215c`.

--- services/portfolio.nix
+++ services/portfolio.nix
@@ -1,7 +1,7 @@
 { system, ... }:
 let
   portfolio = builtins.getFlake
-    "github:jrpotter/portfolio/0f89bdf6c17dd7a61988f8b3629db1988e6f7357";
+    "github:jrpotter/portfolio/426215c12ba709e02dadba5f8d4c944bbdcf90fb";
 in
 {
   services.nginx.virtualHosts."www.jrpotter.com" = {
