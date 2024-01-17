60e7faeb7a9cdee6d6c9a3cf8c88e9566617953d
Joshua Potter <jrpotter2112@gmail.com>
2024-01-17 07:39:08 -0700

    chore: bump `blog` to `9e3821f`.

--- services/blog.nix
+++ services/blog.nix
@@ -1,7 +1,7 @@
 { system, ... }:
 let
   blog = builtins.getFlake
-    "github:jrpotter/blog/3985323a0378ad7571511a348ef83ef833b08646";
+    "github:jrpotter/blog/9e3821f31a1261bffb83392ce02cb9d2a6da9b81";
 in
 {
   services.nginx.virtualHosts."blog.jrpotter.com" = {
