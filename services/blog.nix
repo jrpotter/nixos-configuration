a0fd90c5cfef3492be8c60d858b214ae07f6773d
Joshua Potter <jrpotter2112@gmail.com>
2024-01-19 20:52:16 -0700

    chore: Update `blog` to `26e3857`.

--- services/blog.nix
+++ services/blog.nix
@@ -1,7 +1,7 @@
 { system, ... }:
 let
   blog = builtins.getFlake
-    "github:jrpotter/blog/09fcb0b96a9c503b69d0bccc8f253110dd42ef77";
+    "github:jrpotter/blog/26e385791ccb78823491413fc7d61121716d8ad8";
 in
 {
   services.nginx.virtualHosts."blog.jrpotter.com" = {
