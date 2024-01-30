65904ad5d6e18e602ab13cf0015ad1d85771bb4a
Joshua Potter <jrpotter2112@gmail.com>
2024-01-30 11:47:36 -0700

    Add anki-connect plugin.

--- users/jrpotter/anki.nix
+++ users/jrpotter/anki.nix
@@ -1,11 +1,21 @@
 { pkgs, ... }:
 let
+  addons = id: "Anki2/addons21/${toString id}";
+
   FlexibleGrading = pkgs.fetchFromGitHub {
     owner = "jrpotter";
     repo = "FlexibleGrading";
     rev = "d9cd06bbb154a0740518e58e4619d3855e22c027";
     hash = "sha256-ciIVFyt7TDBeC+h5feA5I17Ld1Pge/hRDqGMMcLTJiM=";
     fetchSubmodules = true;
+  };
+
+  anki-connect = pkgs.fetchFromGitea {
+    domain = "git.foosoft.net";
+    owner = "alex";
+    repo = "anki-connect";
+    rev = "2996476e03a86ea56fd8148e9a434d6f65af890a";
+    hash = "sha256-5kwOZ6BLZqslBeOcX96GwLv3ME2J3czfw8oHG+ZgIQI=";
   };
 
   image-occlusion-enhanced = pkgs.fetchFromGitHub {
@@ -37,16 +47,21 @@ let
   };
 in
 {
-  xdg.dataFile."Anki2/addons21/1715096333" = {
-    source = FlexibleGrading;
-    recursive = true;  # Let's addon write to directory.
+  xdg.dataFile = {
+    "${addons 1715096333}" = {
+      source = FlexibleGrading;
+      recursive = true;  # Let's addon write to directory.
+    };
+
+    "${addons 2055492159}".source =
+      "${anki-connect}/plugin";
+
+    "${addons 1374772155}".source =
+      "${image-occlusion-enhanced}/src/image_occlusion_enhanced";
+
+    "${addons 566351439}".source =
+      syntax-highlighting-ng;
   };
-
-  xdg.dataFile."Anki2/addons21/1374772155".source =
-    "${image-occlusion-enhanced}/src/image_occlusion_enhanced";
-
-  xdg.dataFile."Anki2/addons21/566351439".source =
-    syntax-highlighting-ng;
 
   home.packages = [ pkgs.anki-bin ];
 }
