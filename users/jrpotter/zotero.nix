f43dc19f54742749f1cde842d77fc196aa575fdf
Joshua Potter <jrpotter2112@gmail.com>
2024-02-03 17:17:16 -0700

    Restore Zotero icon.

--- users/jrpotter/zotero.nix
+++ users/jrpotter/zotero.nix
@@ -129,6 +129,10 @@ stdenv.mkDerivation rec {
     # install desktop file and icons.
     mkdir -p $out/share/applications
     cp ${desktopItem}/share/applications/* $out/share/applications/
+    for size in 32 128; do
+      install -Dm444 icons/icon$size.png \
+        $out/share/icons/hicolor/''${size}x''${size}/apps/zotero.png
+    done
 
     runHook postInstall
   '';
