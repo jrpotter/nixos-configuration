471dafd65674f56a7c57a140b35e59b090b33573
Joshua Potter <jrpotter2112@gmail.com>
2024-02-22 14:26:06 -0700

    chore: bump `notebook` to `a42ccc8`.

--- services/notebook.nix
+++ services/notebook.nix
@@ -1,7 +1,7 @@
 { system, ... }:
 let
   notebook = builtins.getFlake
-    "github:jrpotter/notebook/ae7155758923bd9ca371d70ecae1dfbbea744f12";
+    "github:jrpotter/notebook/a42ccc880a7c408ec7f87f6eceeaf9d435e68cc4";
 in
 {
   services.nginx.virtualHosts."notebook.jrpotter.com" = {
